d46323ac37a6a2f9fb2a6bdf8320503e422ca18a
杨俊艺 <1209032479@qq.com>
2017-12-12 12:55:33 +0800

    无

--- TypingGame/Form1.Designer.cs
+++ TypingGame/Form1.Designer.cs
@@ -29,11 +29,79 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(546, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(416, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(288, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- TypingGame/Form1.cs
+++ TypingGame/Form1.cs
@@ -15,6 +15,63 @@ namespace TypingGame
         public Form1()
         {
             InitializeComponent();
+            GameInit();
+        }
+        List<Label> la = new List<Label>();
+        Point[] poin = new Point[4];
+        char[] ch = new char[4];
+        private void GameInit()
+        {
+            Random rd = new Random();
+            la.Add(label1);
+            la.Add(label2);
+            la.Add(label3);
+            la.Add(label4);
+            for (int i = 0; i < 4; i++)
+            {
+                poin[i] = new Point(i * 120 + 50, 50);
+                la[i].Location = poin[i];
+                ch[i] = (char)(rd.Next(0, 26) + 65);
+                la[i].Text = ch[i].ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            poin[0].Y += 2;
+            poin[1].Y += 3;
+            poin[2].Y += 4;
+            poin[3].Y += 2;
+            for (int i = 0; i < 4; i++)
+            {
+                if (poin[i].Y > this.Height)
+                {
+                    poin[i].Y = 0;
+                }
+                la[i].Location = poin[i];
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (e.KeyChar.ToString().ToUpper() == la[i].Text)
+                {
+                    Random rd = new Random();
+                    la[i].Visible = false;
+                    la[i].Text = ((char)(rd.Next(0, 26) + 65)).ToString();
+                    poin[i].Y = 0;
+                    la[i].Visible = true;
+                    break;
+                }
+            }
         }
     }
 }
